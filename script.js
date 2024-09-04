3c2f433e38a47f850a7a4412637b6e2e4fc7e766
bobchopra <bobchopracoding@gmail.com>
2024-09-04 09:46:38 -0400

    function declaration,

---
+++ script.js
@@ -0,0 +1,15 @@
+//function syntax
+// function functionName(parameters or no parameters which is leaveing it empty){
+//     //function body nothing but instructions or logics in the function
+//     return value // optional
+// }
+// functionName(arguments or no arguments which is leaveing it empty) this is how you call a function
+
+//function declaration
+function greet(name){
+    console.log(`hello ${name}`)
+    
+}
+greet("babypizza")
+//function expreession
+//arrow function
